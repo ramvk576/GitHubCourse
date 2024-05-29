279970fbe509a390693ab8a23b027ad259351cfd
ramvk576 <ramvk576@gmail.com>
2024-05-29 15:12:16 +0530

    add/modify/delete

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,5 +4,9 @@ public class LoginPage {
 	
 	public static void main(String a[]) {
 		System.out.println("Hello LP");
+		String un = "Ramesh";
+		String pwd = "password";
+		System.out.println("Logged in successfully");
+		
 	}
 }
---
+++ src/Pages/SearchPage.java
@@ -0,0 +1,10 @@
+package Pages;
+
+public class SearchPage {
+	
+	public static void main()
+	{
+		System.out.println("this is search page");
+	}
+
+}
